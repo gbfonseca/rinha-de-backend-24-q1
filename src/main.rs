4a40e09d492279c089488634084ab67e876f45d0
Gabriel <gabriel.fonsecadev@gmail.com>
2024-02-06 22:36:52 -0300

    feat: implementing find clients

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,9 @@
+use diesel::{Connection, PgConnection};
+
 mod repository;
 pub mod schema;
-
-use diesel::prelude::*;
-
 #[tokio::main] // By default, tokio_postgres uses the tokio crate as its runtime.
 async fn main() {
-    use self::schema::clientes::dsl::*;
-
-    let connection = &mut establish_connection();
-
-    let results = clientes
-        .select(crate::repository::clients::Clients::as_select())
-        .load(connection)
-        .expect("Error loading posts");
-
-    println!("Displaying {} posts", results.len());
-    for client in results {
-        println!("{}", client.nome);
-        println!("-----------\n");
-        println!("{}", client.limite);
-    }
-
     println!("Hello World");
 }
 
--- src/repository/clients.rs
+++ src/repository/clients.rs
@@ -1,11 +1,46 @@
-// pub async fn find_by_id(client: &Client, id: &i32) {}
+use crate::schema::clientes::dsl::*;
 use diesel::prelude::*;
 
-#[derive(Queryable, Selectable)]
+#[derive(Queryable, Selectable, Debug)]
 #[diesel(table_name = crate::schema::clientes)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Clients {
     pub id: i32,
     pub nome: String,
     pub limite: i32,
+}
+
+#[allow(dead_code)]
+impl Clients {
+    pub fn find(connection: &mut PgConnection) -> Vec<Clients> {
+        let results = clientes
+            .select(Clients::as_select())
+            .load(connection)
+            .expect("Error loading clientes");
+
+        results
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use diesel::{Connection, PgConnection};
+
+    use super::*;
+
+    #[test]
+    fn should_find_all_clients() {
+        let connection = &mut establish_connection();
+        let results = Clients::find(connection);
+
+        let first_client = results.get(0).unwrap();
+        assert_eq!(first_client.id, 1);
+        assert_eq!(first_client.nome, "o barato sai caro");
+    }
+
+    pub fn establish_connection() -> PgConnection {
+        let database_url = String::from("postgres://admin:123@localhost/rinha");
+        PgConnection::establish(&database_url)
+            .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+    }
 }
